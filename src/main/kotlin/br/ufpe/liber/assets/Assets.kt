5577cd241a5702feb709693887eafdb5aadc9810
Marcos Pereira <marcos.silva@gmail.com>
2024-01-25 22:30:46 -0500

    assets: Better use of etag, last modified, and accept encoding headers

--- src/main/kotlin/br/ufpe/liber/assets/Assets.kt
+++ src/main/kotlin/br/ufpe/liber/assets/Assets.kt
@@ -13,6 +13,7 @@ import java.lang.IllegalArgumentException
 import java.util.NavigableMap
 import java.util.Optional
 import java.util.TreeMap
+import kotlin.jvm.optionals.getOrDefault
 
 @OptIn(ExperimentalSerializationApi::class)
 @Singleton
@@ -86,12 +87,71 @@ data class Asset(
         return "classpath:public/$resourcePathWithEncoding"
     }
 
-    fun preferredEncodedResource(acceptEncoding: String): Optional<Encoding> = Optional.ofNullable(
-        sortedEncodings.firstOrNull { acceptEncoding.contains(it.http) },
-    )
+    fun preferredEncodedResource(acceptEncoding: String): Optional<Encoding> {
+        val acceptEncodings = AcceptEncoding.parseHeader(acceptEncoding).sorted()
+
+        // Will handle the resource without a Content-Encoding
+        if (acceptEncodings.isEmpty()) return Optional.empty()
+
+        // If none have a quality value, use whatever is "better"
+        // for the server AND supported by the client.
+        return if (acceptEncodings.none { ac -> ac.q.isPresent }) {
+            Optional.ofNullable(sortedEncodings.firstOrNull { acceptEncoding.contains(it.http) })
+        } else {
+            // Return based on supported encodings and quality values
+            // for requested accept-encodings.
+            Optional.ofNullable(
+                supportedEncodings.maxByOrNull { supportedEnc ->
+                    acceptEncodings.firstOrNull { acceptEnc ->
+                        acceptEnc.name == supportedEnc.http
+                    }?.qualityValue ?: 1f
+                },
+            )
+        }
+    }
 }
 
 @Serializable
 data class Encoding(val http: String, val extension: String, val priority: Int) : Comparable<Encoding> {
     override fun compareTo(other: Encoding): Int = this.priority.compareTo(other.priority)
+}
+
+@Suppress("IDENTIFIER_LENGTH") // it is okay to use `q` because it is part of the spec.
+data class AcceptEncoding(val name: String, val q: Optional<Float> = Optional.empty()) : Comparable<AcceptEncoding> {
+    @Suppress("MAGIC_NUMBER")
+    val qualityValue = q.getOrDefault(1f)
+
+    override fun compareTo(other: AcceptEncoding): Int = other.qualityValue.compareTo(this.qualityValue)
+
+    companion object {
+        private val QUALITY_VALUE_FORMAT = "^[0-1](\\.[0-9]{1,3})?$".toRegex()
+        fun parseHeader(header: String): List<AcceptEncoding> = header.split(",")
+            .map { entry -> parseEntry(entry.trim()) }
+            .filter { it.qualityValue != 0f }
+
+        private fun parseEntry(entry: String): AcceptEncoding {
+            val parts = entry.split(";")
+            val name = parts.first()
+            return if (parts.size > 1) {
+                AcceptEncoding(name, parseQualityValue(parts[1]))
+            } else {
+                AcceptEncoding(name)
+            }
+        }
+
+        private fun parseQualityValue(q: String): Optional<Float> {
+            val parts = q.split("=")
+
+            if (parts.size <= 1) return Optional.empty()
+
+            return if (notAcceptableQualityValue(parts[1].trim())) {
+                Optional.empty()
+            } else {
+                Optional.of(parts[1].trim().toFloat())
+            }
+        }
+
+        // Not precise, but good enough to adhere to https://datatracker.ietf.org/doc/html/rfc9110#section-12.4.2
+        private fun notAcceptableQualityValue(value: String): Boolean = !value.matches(QUALITY_VALUE_FORMAT)
+    }
 }
