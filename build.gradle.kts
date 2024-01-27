bf9df1de2fbe9909009a8abbd9ac855dde047fac
Marcos Pereira <marcos.silva@gmail.com>
2024-01-27 17:20:10 -0500

    deps: Update jte to version 3.1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.22-1.0.17"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.2.1"
-    id("gg.jte.gradle") version "3.1.6"
+    id("gg.jte.gradle") version "3.1.9"
     id("io.micronaut.aot") version "4.2.1"
     // Apply GraalVM Native Image plugin. Micronaut already adds it, but
     // adding it explicitly allows to control which version is used.
@@ -147,11 +147,6 @@ diktat {
     inputs {
         include("src/**/*.kt")
         exclude("src/accessibilityTest/**/*.kt")
-    }
-}
-tasks.configureEach {
-    if (name == "diktatFix" || name == "diktatCheck") {
-        notCompatibleWithConfigurationCache("https://github .com/saveourtool/diktat/issues/1732")
     }
 }
 tasks.named("check") {
