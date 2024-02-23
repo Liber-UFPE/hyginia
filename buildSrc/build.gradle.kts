a822438e8d1318967e0170c0f65a904895a4058e
Marcos Pereira <marcos.silva@gmail.com>
2024-02-23 12:10:13 -0500

    build: use gradle frontend plugin instead of node plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,8 +12,13 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("commons-codec:commons-codec:1.16.1")
-    implementation("org.apache.tika:tika-core:2.9.1")
-    implementation("org.apache.tika:tika-parsers-standard-package:2.9.1")
     implementation("org.apache.logging.log4j:log4j-core:2.23.0")
+    implementation("commons-codec:commons-codec:1.16.1")
+
+    implementation("com.lordcodes.turtle:turtle:0.9.0")
+
+    implementation("org.apache.tika:tika-core:2.9.1")
+    implementation("org.apache.tika:tika-parsers-standard-package:2.9.1") {
+        exclude(group = "org.apache.commons", module = "commons-compress")
+    }
 }
