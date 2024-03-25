83e5c80998a3252843af53376d41e82328727051
Marcos Pereira <marcos.silva@gmail.com>
2024-03-25 15:35:38 -0400

    fix(ui): footnotes highlight when clicking

--- src/main/resources/public/javascripts/main.js
+++ src/main/resources/public/javascripts/main.js
@@ -2,28 +2,35 @@ import {initFlowbite, initPopovers, Collapse} from "flowbite";
 
 initFlowbite();
 
-window.onload = () => {
-    const navbarSearchButton = document.getElementById("navbar-search-button");
-    navbarSearchButton.onclick = () => {
-        const queryInput = document.getElementById("sm-query-input");
-        queryInput.focus();
-    };
-};
+function setupFootnotesAnimationEvent() {
+    const pageContents = document.querySelector(".day-contents");
+    if (pageContents) {
+        pageContents
+            .querySelectorAll(".footnote-ref")
+            .forEach(note => {
+                note.addEventListener("click", () => {
+                    const noteId = note.href.split("#")[1];
+                    const noteElement = document.getElementById(noteId);
+                    if (noteElement) {
+                        noteElement.classList.add("highlight-footnote");
+                        // Remove the class after sometime so that the same footnote
+                        // will blink when user clicks it a second time.
+                        setTimeout(() => {noteElement.classList.remove("highlight-footnote");}, 3000);
+                    }
+                });
+            });
+    }
+}
 
-document.documentElement.addEventListener("htmx:afterRequest", () => {
-    initPopovers();
-});
-
-// https://htmx.org/events/#htmx:afterRequest
-document.documentElement.addEventListener("htmx:afterRequest", () => {
+function showRequestProgress() {
     const indicator = document.getElementById("brand-button");
     indicator.classList.add("htmx-request");
 
     // If the request completes too fast, this avoids the indicator to just flicking.
     setTimeout(() => indicator.classList.remove("htmx-request"), 1300);
-});
+}
 
-document.documentElement.addEventListener("htmx:afterRequest", () => {
+function collapseNavigation() {
     const $targetEl = document.getElementById("navbar-search");
 
     const instanceOptions = {
@@ -33,4 +40,20 @@ document.documentElement.addEventListener("htmx:afterRequest", () => {
 
     const collapse = new Collapse($targetEl, null, {}, instanceOptions);
     collapse.collapse();
-});
+}
+
+window.onload = () => {
+    const navbarSearchButton = document.getElementById("navbar-search-button");
+    navbarSearchButton.onclick = () => {
+        const queryInput = document.getElementById("sm-query-input");
+        queryInput.focus();
+    };
+};
+
+window.addEventListener("load", setupFootnotesAnimationEvent);
+
+// https://htmx.org/events/#htmx:afterRequest
+document.documentElement.addEventListener("htmx:afterRequest", setupFootnotesAnimationEvent);
+document.documentElement.addEventListener("htmx:afterRequest", initPopovers);
+document.documentElement.addEventListener("htmx:afterRequest", showRequestProgress);
+document.documentElement.addEventListener("htmx:afterRequest", collapseNavigation);
