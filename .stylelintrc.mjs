d386e9c4ad74a73fdb240626ba0a5b8f505e7621
Marcos Pereira <marcos.silva@gmail.com>
2024-04-08 23:31:02 -0400

    chores(updates): migrate eslint configuration to new format

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,18 +1,18 @@
 // DO NOT EDIT: this file is automatically synced from the template repository
 // in https://github.com/Liber-UFPE/project-starter.
-/** @type {import('stylelint').Config} */
+/** @type {import("stylelint").Config} */
 export default {
-  extends: "stylelint-config-standard-scss",
-  rules: {
-    "no-descending-specificity": null,
-    "scss/at-rule-no-unknown": [
-      true,
-      {
-        "ignoreAtRules": [
-          "tailwind",
-          "screen"
+    extends: "stylelint-config-standard-scss",
+    rules: {
+        "no-descending-specificity": null,
+        "scss/at-rule-no-unknown": [
+            true,
+            {
+                "ignoreAtRules": [
+                    "tailwind",
+                    "screen"
+                ]
+            }
         ]
-      }
-    ]
-  }
+    }
 };
