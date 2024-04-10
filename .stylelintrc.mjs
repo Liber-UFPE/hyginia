be01a058ace33f5e2c48982cee3010828fbd5d8a
marcospereira <marcos.silva@gmail.com>
2024-04-10 00:07:23 +0000

    🔄 synced file(s) with Liber-UFPE/project-starter

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,6 +1,6 @@
 // DO NOT EDIT: this file is automatically synced from the template repository
 // in https://github.com/Liber-UFPE/project-starter.
-/** @type {import("stylelint").Config} */
+/** @type {import('stylelint').Config} */
 export default {
     extends: "stylelint-config-standard-scss",
     rules: {
