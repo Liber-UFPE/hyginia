e2440da65ae80ff35b35a6ebaacf78656c2c3f36
marcospereira <marcos.silva@gmail.com>
2024-04-26 16:14:03 +0000

    🔄 synced file(s) with Liber-UFPE/project-starter

--- src/main/kotlin/br/ufpe/liber/views/ViewsHelper.kt
+++ src/main/kotlin/br/ufpe/liber/views/ViewsHelper.kt
@@ -3,18 +3,27 @@ package br.ufpe.liber.views
 import gg.jte.html.HtmlContent
 import gg.jte.html.HtmlTemplateOutput
 import io.micronaut.http.context.ServerRequestContext
+import java.util.Optional
 
 // DO NOT EDIT: this file is automatically synced from the template repository
 // in https://github.com/Liber-UFPE/project-starter.
 
 object ViewsHelper {
-    fun isHtmxRequest(): Boolean = ServerRequestContext
+    const val HX_TARGET_HEADER: String = "HX-Target"
+    const val HX_REQUEST_HEADER: String = "HX-Request"
+
+    fun htmxTarget(): Optional<Pair<String, String>> = ServerRequestContext
         .currentRequest<Any>()
-        .map { io.micronaut.views.htmx.http.HtmxRequestUtils.isHtmxRequest(it) }
-        .orElse(false)
+        .flatMap { Optional.ofNullable(it.headers[HX_TARGET_HEADER]) }
+        .map { Pair(HX_TARGET_HEADER, it) }
 
+    fun htmxRequest(): Optional<Pair<String, Boolean>> = ServerRequestContext
+        .currentRequest<Any>()
+        .flatMap { Optional.ofNullable(it.headers[HX_REQUEST_HEADER]) }
+        .map { Pair(HX_REQUEST_HEADER, it.toBoolean()) }
+
+    fun isHtmxRequest(): Boolean = htmxRequest().map { it.second }.orElse(false)
     fun notHtmxRequest(): Boolean = !isHtmxRequest()
-
     fun isActive(path: String): Boolean = ServerRequestContext.currentRequest<Any>()
         .map { req -> req.path == path }
         .orElse(false)
--- src/test/kotlin/br/ufpe/liber/views/ViewsHelperTest.kt
+++ src/test/kotlin/br/ufpe/liber/views/ViewsHelperTest.kt
@@ -2,11 +2,12 @@ package br.ufpe.liber.views
 
 import br.ufpe.liber.asString
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.optional.shouldBePresent
+import io.kotest.matchers.optional.shouldNotBePresent
 import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.context.ServerRequestContext
-import io.micronaut.views.htmx.http.HtmxRequestHeaders
 import io.mockk.clearStaticMockk
 import io.mockk.every
 import io.mockk.mockk
@@ -29,11 +30,11 @@ class ViewsHelperTest : BehaviorSpec({
     }
 
     given("ViewHelpers") {
-        `when`("HX-Request header is present") {
+        `when`("HX-Target header is present") {
             beforeTest {
                 setupCurrentRequest { req ->
                     val headers: HttpHeaders = mockk()
-                    every { headers[HtmxRequestHeaders.HX_REQUEST] } answers { "true" }
+                    every { headers[ViewsHelper.HX_TARGET_HEADER] } answers { "main-content" }
                     every { req.headers } answers { headers }
                 }
             }
@@ -41,9 +42,82 @@ class ViewsHelperTest : BehaviorSpec({
             afterTest {
                 clearStaticMockk(ServerRequestContext::class)
             }
+
+            then("should return its value") {
+                ViewsHelper.htmxTarget().shouldBePresent { value ->
+                    value.first shouldBe ViewsHelper.HX_TARGET_HEADER
+                    value.second shouldBe "main-content"
+                }
+            }
+        }
+
+        `when`("HX-Target header is NOT present") {
+            beforeTest {
+                setupCurrentRequest { req ->
+                    val headers: HttpHeaders = mockk()
+                    every { headers[ViewsHelper.HX_TARGET_HEADER] } answers { null }
+                    every { req.headers } answers { headers }
+                }
+            }
+
+            afterTest {
+                clearStaticMockk(ServerRequestContext::class)
+            }
+
+            then("should return empty value") {
+                ViewsHelper.htmxTarget().shouldNotBePresent()
+            }
+        }
+
+        `when`("HX-Request header value is \"true\"") {
+            beforeTest {
+                setupCurrentRequest { req ->
+                    val headers: HttpHeaders = mockk()
+                    every { headers[ViewsHelper.HX_REQUEST_HEADER] } answers { "true" }
+                    every { req.headers } answers { headers }
+                }
+            }
+
+            afterTest {
+                clearStaticMockk(ServerRequestContext::class)
+            }
+
+            then("should return its value") {
+                ViewsHelper.htmxRequest().shouldBePresent { value ->
+                    value.first shouldBe ViewsHelper.HX_REQUEST_HEADER
+                    value.second shouldBe true
+                }
+            }
+
             then("should be a turbo request") {
                 ViewsHelper.isHtmxRequest() shouldBe true
                 ViewsHelper.notHtmxRequest() shouldBe false
+            }
+        }
+
+        `when`("HX-Request header value is \"false\"") {
+            beforeTest {
+                setupCurrentRequest { req ->
+                    val headers: HttpHeaders = mockk()
+                    every { headers[ViewsHelper.HX_REQUEST_HEADER] } answers { "false" }
+                    every { req.headers } answers { headers }
+                }
+            }
+
+            afterTest {
+                clearStaticMockk(ServerRequestContext::class)
+            }
+
+            then("should return its value") {
+                ViewsHelper.htmxRequest().shouldBePresent { value ->
+                    value.first shouldBe ViewsHelper.HX_REQUEST_HEADER
+                    value.second shouldBe false
+                }
+            }
+
+            then("should be a turbo request") {
+                ViewsHelper.isHtmxRequest() shouldBe false
+                ViewsHelper.notHtmxRequest() shouldBe true
             }
         }
 
@@ -51,9 +125,13 @@ class ViewsHelperTest : BehaviorSpec({
             beforeTest {
                 setupCurrentRequest { req ->
                     val headers: HttpHeaders = mockk()
-                    every { headers[HtmxRequestHeaders.HX_REQUEST] } answers { null }
+                    every { headers[ViewsHelper.HX_REQUEST_HEADER] } answers { null }
                     every { req.headers } answers { headers }
                 }
+            }
+
+            then("should return empty value") {
+                ViewsHelper.htmxRequest().shouldNotBePresent()
             }
 
             then("it should not be a htmx request") {
